f00d96bb0238c0f344d296fb1e15ebea6680f8f0
baguscs <cahyob59@gmail.com>
2023-07-07 13:45:24 +0700

    make view home

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,5 @@
-import './bootstrap';
+import "https://code.jquery.com/jquery-3.7.0.min.js";
+import "./bootstrap";
+import "../js/jquery.scrollspy.js";
+import "../js/script.js";
+import.meta.glob(["../images/**"]);
